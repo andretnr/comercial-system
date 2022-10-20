b6d01b408d4e8e493582485c4db10f2641d38f9f
andre <andretnr@gmail.com>
2022-10-19 21:50:32 -0300

    feat: applying integration

--- conta-produtos/src/main/java/com/nunes/contaprodutos/services/PrductRequestService.java
+++ conta-produtos/src/main/java/com/nunes/contaprodutos/services/PrductRequestService.java
@@ -1,14 +1,32 @@
 package com.nunes.contaprodutos.services;
 
-import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.nunes.contaprodutos.entities.Product;
 import com.nunes.contaprodutos.entities.ProductRequest;
 
 @Service
 public class PrductRequestService {
+	
+	@Value("${lista-produtos.host}")
+	private String pdHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public ProductRequest getEstoqueTotalValue(Long productId,int requestedQuantity) {
-		return new ProductRequest("Lápis", "preto", 10, requestedQuantity, "0.5");
+		
+		Map<String, String> uri = new HashMap();
+		uri.put("id", String.valueOf(productId));
+		Product product = restTemplate.getForObject(pdHost + "/products/{id}", Product.class, uri);
+		
+		return new ProductRequest(product.getName(), product.getDescription(), product.getQuantity(), requestedQuantity, product.getPrice().toString());
 		
 	}
 }
